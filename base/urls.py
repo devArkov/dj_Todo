0f11cf3dbfa0e09ceb6317c3e0a362fa86fb820f
Alexandr Korobov <devarko@sapo.pt>
2023-01-04 18:10:03 +0300

    Create, update and delete tasks

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.tasklist, name='tasks'),
+    path('', views.TaskListView.as_view(), name='tasks'),
+    path('task/<int:pk>/', views.TaskDetailView.as_view(), name='task'),
+    path('create-task/', views.TaskCreateView.as_view(), name='task-create'),
+    path('edit-task/<int:pk>/', views.TaskUpdateView.as_view(), name='task-edit'),
+    path('delete-task/<int:pk>/', views.TaskDelete.as_view(), name='task-delete'),
+
 ]
--- base/views.py
+++ base/views.py
@@ -1,8 +1,38 @@
-from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView,DeleteView
+from django.urls import reverse_lazy
+
+from .models import Task
 
 
 # Create your views here.
-def tasklist(request: HttpRequest) -> HttpResponse:
-    return HttpResponse('Tasklist')
+class TaskListView(ListView):
+    model = Task
+    template_name = 'base/task_list.html'
+    context_object_name = 'tasks'
 
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'base/task.html'
+    context_object_name = 'task'
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    fields = '__all__'
+    template_name = 'base/edit_task.html'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
